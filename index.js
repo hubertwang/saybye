1a229db00173e7459f3e4094d7b13099ec82692b
Hubert Wang <hubert_wang@trend.com.tw>
2018-04-09 11:00:03 +0800

    [JS] Add param name

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Bye Bye!');
+let name = null;
+if (process && process.argv && Array.isArray(process.argv) && process.argv.length > 2) {
+  name = process.argv[2];
+}
+
+if (!name)
+  console.log('Bye Bye!');
+else
+  console.log(name + ' Bye Bye!');
