616fc91556a56d968e01ed2782f4f3739e8aca16
Hubert Wang <hubert_wang@trend.com.tw>
2018-04-09 14:08:06 +0800

    Revert say package

--- saybye.js
+++ saybye.js
@@ -1,7 +1,5 @@
 #! /usr/bin/env node
 
-// const say = require('say');
-
 let name = null;
 if (process && process.argv && Array.isArray(process.argv) && process.argv.length > 2) {
   name = process.argv[2];
@@ -16,4 +14,3 @@ else
   message = name + ' ' + greeting;
 
 console.log(message);
-// say.speak(message);
